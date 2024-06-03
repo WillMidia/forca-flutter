d71502b0e0eaa6c085ec4e868b7e714b7dc798c4
WillMidia <63423575+WillMidia@users.noreply.github.com>
2024-06-03 14:27:13 -0300

    Icones, tela inicial e congratulação

--- lib/congratulation_screen.dart
+++ lib/congratulation_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CongratulationsScreen extends StatefulWidget {
+  final String word;
   final String description;
   final VoidCallback onPlayAgain;
 
-  CongratulationsScreen({required this.description, required this.onPlayAgain});
+  CongratulationsScreen({
+    required this.word,
+    required this.description,
+    required this.onPlayAgain,
+  });
 
   @override
   _CongratulationsScreenState createState() => _CongratulationsScreenState();
@@ -53,57 +58,95 @@ class _CongratulationsScreenState extends State<CongratulationsScreen>
       body: Center(
         child: ScaleTransition(
           scale: _animationScale,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.star,
-                size: 100,
-                color: Colors.yellow,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Parabéns, você encontrou todas as palavras!',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.emoji_emotions, // Updated icon to a smiling face emoji
+                  size: 100,
+                  color: Colors.yellow,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 10),
-              Text(
-                widget.description, // Aqui é onde exibimos a descrição
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  _controller.reverse().then((_) {
-                    widget.onPlayAgain();
-                  });
-                },
-                child: Text(
-                  'Jogar Novamente',
+                SizedBox(height: 20),
+                Text(
+                  'Parabéns!',
                   style: TextStyle(
-                    fontSize: 18,
+                    fontSize: 32,
                     fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Você adivinhou a palavra:',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                // Verifique se o widget está sendo reconstruído com os novos valores
+                Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                      decoration: BoxDecoration(
+                        color: Color(0xFF006994),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            widget.word.toUpperCase(), // A palavra deve ser exibida aqui
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            widget.description, // A descrição deve ser exibida aqui
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: () {
+                    _controller.reverse().then((_) {
+                      widget.onPlayAgain();
+                    });
+                  },
+                  child: Text(
+                    'Jogar Novamente',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF006994),
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
                 ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF006994),
-                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -21,10 +21,40 @@ class _GameScreenState extends State<GameScreen> {
     startNewGame();
   }
 
+  String normalizeWord(String word) {
+    return word
+        .replaceAll('ã', 'a')
+        .replaceAll('á', 'a')
+        .replaceAll('â', 'a')
+        .replaceAll('é', 'e')
+        .replaceAll('ê', 'e')
+        .replaceAll('í', 'i')
+        .replaceAll('õ', 'o')
+        .replaceAll('ó', 'o')
+        .replaceAll('ô', 'o')
+        .replaceAll('ú', 'u')
+        .replaceAll('ç', 'c');
+  }
+
+  String normalizeLetter(String letter) {
+    return letter
+        .replaceAll('ã', 'a')
+        .replaceAll('á', 'a')
+        .replaceAll('â', 'a')
+        .replaceAll('é', 'e')
+        .replaceAll('ê', 'e')
+        .replaceAll('í', 'i')
+        .replaceAll('õ', 'o')
+        .replaceAll('ó', 'o')
+        .replaceAll('ô', 'o')
+        .replaceAll('ú', 'u')
+        .replaceAll('ç', 'c');
+  }
+
   void startNewGame() {
     final randomIndex = (wordList.keys.toList()..shuffle()).first;
     setState(() {
-      word = randomIndex.toUpperCase();
+      word = normalizeWord(randomIndex.toUpperCase());
       description = wordList[randomIndex]!;
       displayWord = '_' * word.length;
       lives = 6;
@@ -36,10 +66,13 @@ class _GameScreenState extends State<GameScreen> {
   void guessLetter(String letter) {
     setState(() {
       letter = letter.toUpperCase();
-      if (word.contains(letter)) {
+      String normalizedLetter = normalizeLetter(letter);
+      String normalizedWord = normalizeWord(word);
+
+      if (normalizedWord.contains(normalizedLetter)) {
         for (int i = 0; i < word.length; i++) {
-          if (word[i] == letter) {
-            displayWord = displayWord.substring(0, i) + letter + displayWord.substring(i + 1);
+          if (normalizeLetter(word[i]) == normalizedLetter) {
+            displayWord = displayWord.substring(0, i) + word[i] + displayWord.substring(i + 1);
           }
         }
       } else {
@@ -54,6 +87,7 @@ class _GameScreenState extends State<GameScreen> {
           context,
           MaterialPageRoute(
             builder: (context) => CongratulationsScreen(
+              word: word, // Pass the correct word here
               description: description,
               onPlayAgain: () {
                 Navigator.pop(context); // Sair da tela de parabéns
@@ -85,6 +119,27 @@ class _GameScreenState extends State<GameScreen> {
     });
   }
 
+IconData getEmotionIcon() {
+  switch (lives) {
+    case 6:
+      return Icons.sentiment_very_satisfied;
+    case 5:
+      return Icons.sentiment_satisfied;
+    case 4:
+      return Icons.sentiment_neutral;
+    case 3:
+      return Icons.sentiment_dissatisfied;
+    case 2:
+      return Icons.sentiment_very_dissatisfied;
+    case 1:
+      return Icons.sentiment_very_dissatisfied_sharp; // Ícone de chorar
+    case 0:
+      return Icons.sentiment_very_dissatisfied_outlined; // Ícone de chorar
+    default:
+      return Icons.sentiment_very_dissatisfied;
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
@@ -92,66 +147,84 @@ class _GameScreenState extends State<GameScreen> {
     final screenWidth = mediaQuery.size.width;
 
     return Scaffold(
-  appBar: AppBar(
-    title: Text('Forca do Oceano'),
-    backgroundColor: Color(0xFF006994),
-  ),
-  body: SingleChildScrollView(
-    child: Container(
-      color: Color.fromARGB(255, 1, 81, 112),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-            Container(
-              height: screenHeight * 0.4,
-              child: Center(
-                child: Text(
-                  displayWord.split('').join(' '),
-                  style: TextStyle(
-                    fontSize: 48,
-                    color: Colors.white,
-                    letterSpacing: 4.0,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Vidas restantes: $lives',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(height: 20),
-            Wrap(
-              spacing: 8.0,
-              runSpacing: 8.0,
-              children: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((letter) {
-                bool isGuessed = guessedLetters.contains(letter);
-                bool isPressed = pressedLetters.contains(letter);
-                return ElevatedButton(
-                  onPressed: isPressed ? null : () => guessLetter(letter),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: isGuessed ? Colors.grey : Color.fromARGB(255, 0, 135, 189),
-                    shape: CircleBorder(),
-                    padding: EdgeInsets.all(15),
-                  ),
-                  child: Text(
-                    letter,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                );
-              }).toList(),
-            ),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(getEmotionIcon(), color: Colors.white),
+            SizedBox(width: 10),
+            Text('Forca do Oceano'),
           ],
         ),
+        backgroundColor: Color(0xFF006994),
       ),
-    ),
+      body: Container(
+        color: Color.fromARGB(255, 1, 81, 112),
+        width: double.infinity,
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: screenHeight * 0.4,
+                alignment: Alignment.center,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      getEmotionIcon(),
+                      size: 100,
+                      color: Colors.white,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      displayWord.split('').join(' '),
+                      style: TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                        letterSpacing: 4.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Vidas restantes: $lives',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              Wrap(
+                spacing: 8.0,
+                runSpacing: 8.0,
+                children: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((letter) {
+                  bool isGuessed = guessedLetters.contains(letter);
+                  bool isPressed = pressedLetters.contains(letter);
+                  return ElevatedButton(
+                    onPressed: isPressed ? null : () => guessLetter(letter),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: isGuessed ? Colors.grey : Color.fromARGB(255, 0, 135, 189),
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(15),
+                    ),
+                    child: Text(
+                      letter,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
